92c5b1dbc362cfbda2e6dfdefb25b446d25896ca
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-03-19 16:45:36 -0300

    feat: add install pwa btn

--- js/index.js
+++ js/index.js
@@ -2,16 +2,49 @@ function loadCss() {
   document.body.style.visibility = 'visible';
 }
 
-// Inicialize o deferredPrompt para posteriormente mostrar o prompt de instalação do navegador.
-let deferredPrompt;
+window.addEventListener('load', () => {
+  // Inicialize o deferredPrompt para posteriormente mostrar o prompt de instalação do navegador.
+  let deferredPrompt;
+  let pwaInstallButton = document.getElementById('btn-install-pwa');
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Impede que o mini-infobar apareça em mobile
-  e.preventDefault();
-  // Guarda evento para que possa ser disparado depois.
-  deferredPrompt = e;
-  // Atualiza UI notifica usuário que pode instalar PWA
-  showInstallPromotion();
-  // Opcionalmente, enviar eventos de analytics que promo de instalação PWA foi mostrado.
-  console.log(`'beforeinstallprompt' event was fired.`);
+  function showInstallPromotion() {
+    pwaInstallButton.style.display = 'block';
+  }
+
+  function hideInstallPromotion() {
+    pwaInstallButton.style.display = 'none';
+  }
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Impede que o mini-infobar apareça em mobile
+    e.preventDefault();
+    // Guarda evento para que possa ser disparado depois.
+    deferredPrompt = e;
+    // Atualiza UI notifica usuário que pode instalar PWA
+    showInstallPromotion();
+    // Opcionalmente, enviar eventos de analytics que promo de instalação PWA foi mostrado.
+    console.log(`'beforeinstallprompt' event was fired.`);
+  });
+
+  pwaInstallButton.addEventListener('click', async () => {
+    // Esconde a promoção de instalação fornecida pelo app
+    hideInstallPromotion();
+    // Mostra prompt de instalação
+    deferredPrompt.prompt();
+    // Espera usuário responder ao prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Opcionalmente, enviar evento analytics com resultado da escolha do usuário
+    console.log(`User response to the install prompt: ${outcome}`);
+    // Usamos o prompt e não podemos usar de novo; jogue fora
+    deferredPrompt = null;
+  });
+
+  window.addEventListener('appinstalled', () => {
+    // Esconder a promoção de instalação fornecida pela app
+    hideInstallPromotion();
+    // Limpar o deferredPrompt para que seja coletado
+    deferredPrompt = null;
+    // Opcionalmente, enviar evento de analytics para indicar instalação com sucesso
+    console.log('PWA was installed');
+  });
 });
