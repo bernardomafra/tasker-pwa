60b1eb105c1c476ec8de7238429500accd4cfae8
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-03-19 16:28:56 -0300

    before install sw

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 function loadCss() {
   document.body.style.visibility = 'visible';
 }
+
+// Inicialize o deferredPrompt para posteriormente mostrar o prompt de instalação do navegador.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Impede que o mini-infobar apareça em mobile
+  e.preventDefault();
+  // Guarda evento para que possa ser disparado depois.
+  deferredPrompt = e;
+  // Atualiza UI notifica usuário que pode instalar PWA
+  showInstallPromotion();
+  // Opcionalmente, enviar eventos de analytics que promo de instalação PWA foi mostrado.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
