6eecf0657e580dafe5315a1d75efc5945dfa366d
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-03-19 15:31:39 -0300

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,29 @@
-self.addEventListener('install', function (event) {
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+window.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('pwa-cache').then(function (cache) {
       return cache.add('index.html');
@@ -6,7 +31,7 @@ self.addEventListener('install', function (event) {
   );
 });
 
-self.addEventListener('fetch', function (event) {
+window.addEventListener('fetch', function (event) {
   event.repondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
