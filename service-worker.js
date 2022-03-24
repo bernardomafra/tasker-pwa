5e634e8659e4d211cc04465420e82841160d26fb
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-03-24 01:01:41 -0300

    fix: fetch cache sw

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'tasker-pwa-v2.8';
+const cacheName = 'tasker-pwa-v3.0.0';
 
 const htmlFiles = ['/index.html', '/new-list.html'];
 
@@ -58,41 +58,48 @@ const pathsToCache = [
   ...assetsFiles,
 ];
 
-// Armazena todos os arquivos no cache atual
-self.addEventListener('install', function (event) {
-  caches.open(cacheName).then((cache) => {
-    cache.addAll(pathsToCache);
-  });
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(pathsToCache)),
+  );
 });
 
-// Recupera todos os nomes de cache e apaga aqueles
-// que forem diferentes do cache atual
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        }),
-      );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.open(currentVersion).then((cache) => {
+      return cache.match(e.request).then((resp) => {
+        // Request found in current cache, or fetch the file
+        return (
+          resp ||
+          fetch(e.request)
+            .then((response) => {
+              // Cache the newly fetched file for next time
+              cache.put(e.request, response.clone());
+              return response;
+              // Fetch failed, user is offline
+            })
+            .catch(() => {
+              // Look in the whole cache to load a fallback version of the file
+              return caches.match(e.request).then((fallback) => {
+                return fallback;
+              });
+            })
+        );
+      });
     }),
   );
 });
 
-// Tenta servir o arquivo do cache atual. Se não for possível,
-// baixa o recurso da web e o armazena localmente, antes de entregar
-// uma cópia para o usuário.
-self.addEventListener('fetch', function (event) {
-  let resposta = caches.open(cacheName).then((cache) => {
-    return cache.match(event.request).then((recurso) => {
-      if (recurso) return recurso;
-      return fetch(event.request).then((recurso) => {
-        cache.put(event.request, recurso.clone());
-        return recurso;
-      });
-    });
-  });
-  event.respondWith(resposta);
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keyList) =>
+        Promise.all(
+          keyList.map((key) =>
+            key != cacheName ? caches.delete(key) : Promise.resolve(),
+          ),
+        ),
+      ),
+  );
 });
